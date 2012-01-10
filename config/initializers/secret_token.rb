bcc3cadaabd71e1afbf7e1ce547fc1f10ea418b3
Dan Berk <dan@lastpictures.com>
2012-01-09 20:59:11 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Danberk::Application.config.secret_token = '57abb002fb18a0ceaf11d495a4fb37ecaaf33f598fd1083a260961faed9eda3a97ecb29fd9538831a9e7f273c56488ce210c7a0c7b533903cdc1e48d0e376f07'
